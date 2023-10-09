9df6188c6e3b02b4b5606acb6fe8fee6a68c2e01
Gen1usDeveloper <jawadkhankhichi582@gmail.com>
2023-10-09 10:30:28 +0500

    Working App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import {Route, Routes} from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import "./styles.css";
 
-
-export default function App(){
+export default function App() {
   return (
     <>
     <Routes>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,13 @@
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
-import Main from "../components/main/main";
+//import Main from "../components/main/main";
 
 const Homepage =()=> {
     return (
         <>
         <Header />
-        <Main />
         <Footer />
         </>
     );
 };
-
 export default Homepage;
